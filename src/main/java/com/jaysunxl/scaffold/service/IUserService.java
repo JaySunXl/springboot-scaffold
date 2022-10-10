2f75967bf23cde0d574b216aeaac23b8d3bf84ab
JaySunXl <sunzt2017@foxmail.com>
2022-10-10 15:03:53 +0800

    mysql

---
+++ src/main/java/com/jaysunxl/scaffold/service/IUserService.java
@@ -0,0 +1,9 @@
+package com.jaysunxl.scaffold.service;
+
+/**
+ * @author sunxind
+ * @create 2022-09-28
+ * @description
+ */
+public interface IUserService {
+}
---
+++ src/main/java/com/jaysunxl/scaffold/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.jaysunxl.scaffold.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author sunxind
+ * @create 2022-09-28
+ * @description
+ */
+@Service
+public class UserServiceImpl {
+}
