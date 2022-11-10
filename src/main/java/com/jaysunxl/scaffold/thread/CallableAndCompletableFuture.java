2ff013983cccb15bf58bb06e52e3594256482114
JaySunXl <sunzt2017@foxmail.com>
2022-11-10 15:06:19 +0800

    异步执行代码的初步使用

--- src/main/java/com/jaysunxl/scaffold/thread/CallableAndCompletableFuture.java
+++ src/main/java/com/jaysunxl/scaffold/thread/CallableAndCompletableFuture.java
@@ -9,12 +9,24 @@ import static cn.hutool.core.thread.ThreadUtil.sleep;
 /**
  * jdk1.8提供
  * 通过回调的方式来处理计算结果，实现了异步非阻塞，性能更优。
- *
+ * 常用api
+ * 1. runAsync/supplyAsync
+ * 2. thenApply/thenApplyAsync
+ *  1. thenApply 由执行job1的线程立即执行job2，即两个job都是同一个线程执行的
+ *  2. thenApplyAsync 将job2提交到线程池中异步执行，实际执行job2的线程可能是另外一个线程
+ * 3. thenAccept/thenRun
+ *  1. thenAccept 同 thenApply 接收上一个任务的返回值作为参数，但是无返回值；
+ *  2. thenRun 方法没有入参，也没有返回值，
+ * 4. exceptionally/whenComplete/handle
+ *  1. whenComplete 未发生异常正常返回值发生异常就返回异常
+ *  2. exceptionally 确认会发生异常
+ *  3. handle 基本一致，区别在于handle有返回值
  * @author sunxind
  * @date 2022-11-10
  */
 public class CallableAndCompletableFuture {
     public static void main(String[] args) {
+        //任务1 创建不带返回值的异步任务
         CompletableFuture<Void> f1 =
                 CompletableFuture.runAsync(() -> {
                     System.out.println("T1:洗水壶...");
@@ -24,7 +36,7 @@ public class CallableAndCompletableFuture {
                     sleep(15, TimeUnit.SECONDS);
                 });
 
-        //任务2：洗茶壶->洗茶杯->拿茶叶
+        //任务2：洗茶壶->洗茶杯->拿茶叶  创建带返回值的异步任务
         CompletableFuture<String> f2 =
                 CompletableFuture.supplyAsync(() -> {
                     System.out.println("T2:洗茶壶...");
