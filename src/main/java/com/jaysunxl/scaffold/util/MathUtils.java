8ac92a40d90833a38f4041507a321928f510715c
JaySunXl <sunzt2017@foxmail.com>
2022-11-22 21:55:29 +0800

    utils

--- src/main/java/com/jaysunxl/scaffold/util/MathUtils.java
+++ src/main/java/com/jaysunxl/scaffold/util/MathUtils.java
@@ -5,5 +5,10 @@ package com.jaysunxl.scaffold.util;
  * @date 2022-11-22
  */
 public class MathUtils {
-
+    public static void main(String[] args) {
+        int list = 1010;
+        System.out.println(list%100);
+        int n = list % 100 == 0 ? list/100:list/100 + 1;
+        System.out.println(n);
+    }
 }
--- src/main/java/com/jaysunxl/scaffold/util/TimeUtils.java
+++ src/main/java/com/jaysunxl/scaffold/util/TimeUtils.java
@@ -1,5 +1,6 @@
 package com.jaysunxl.scaffold.util;
 
+import java.sql.Date;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -9,7 +10,8 @@ import java.time.format.DateTimeFormatter;
  */
 public class TimeUtils {
     public static void main(String[] args) {
-        String format = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        //String format = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        //System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        System.out.println(Date.valueOf(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))));
     }
 }
