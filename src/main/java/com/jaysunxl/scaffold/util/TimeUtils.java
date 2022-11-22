53793b1928f4d259a528cd1a560d5cebdbbe32f2
JaySunXl <sunzt2017@foxmail.com>
2022-11-22 15:22:26 +0800

    时间格式转换

---
+++ src/main/java/com/jaysunxl/scaffold/util/TimeUtils.java
@@ -0,0 +1,14 @@
+package com.jaysunxl.scaffold.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author sunxind
+ * @date 2022-11-22
+ */
+public class TimeUtils {
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    }
+}
