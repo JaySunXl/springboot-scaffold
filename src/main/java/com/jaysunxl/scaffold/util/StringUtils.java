03808894b0a2c12c4e050815000c0f409cce4ff1
JaySunXl <sunzt2017@foxmail.com>
2022-12-07 14:36:32 +0800

    utils

---
+++ src/main/java/com/jaysunxl/scaffold/util/StringUtils.java
@@ -0,0 +1,22 @@
+package com.jaysunxl.scaffold.util;
+
+import java.util.Locale;
+
+/**
+ * @Description
+ * @Author sunxind
+ * @Version 1.0.0
+ * @Date 2022/12/7
+ */
+public class StringUtils {
+
+    /**
+     * 字符串小写
+     *
+     * @param str
+     * @return
+     */
+    public static String toLowerCase(String str) {
+        return str.toLowerCase(Locale.ROOT);
+    }
+}
