e6b2626addb0c8e4c731b3c83167cb3c3b150340
JaySunXl <sunzt2017@foxmail.com>
2022-12-07 09:33:57 +0800

    utils

--- src/main/java/com/jaysunxl/scaffold/util/TimeUtils.java
+++ src/main/java/com/jaysunxl/scaffold/util/TimeUtils.java
@@ -1,11 +1,8 @@
 package com.jaysunxl.scaffold.util;
 
-import java.sql.Date;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 /**
  * @author sunxind
@@ -18,6 +15,9 @@ public class TimeUtils {
         //System.out.println(Date.valueOf(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))));
 
         //System.out.println(new Timestamp(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2022-01-01 21:00:00").getTime()));
-        System.out.println(Timestamp.valueOf(LocalDateTime.now()));
+        //System.out.println(Timestamp.valueOf(LocalDateTime.now()));
+        String data = "null";
+        Timestamp dataDate = new Timestamp(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(data).getTime());
+        System.out.println(dataDate);
     }
 }
