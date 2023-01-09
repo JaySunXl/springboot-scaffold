68109e5171086854d4942487d557b2eeb65882cf
sunxind <sunxind@yonyou.com>
2023-01-09 18:31:37 +0800

    utils

--- src/main/java/com/jaysunxl/scaffold/util/TimeUtils.java
+++ src/main/java/com/jaysunxl/scaffold/util/TimeUtils.java
@@ -1,14 +1,24 @@
 package com.jaysunxl.scaffold.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author JaySunXl
  * @date 2022-11-22
  */
+@Slf4j
 public class TimeUtils {
+
+    public static String convertToStr(LocalDateTime time) {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
     public static void main(String[] args) throws ParseException {
         //String format = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         //System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
